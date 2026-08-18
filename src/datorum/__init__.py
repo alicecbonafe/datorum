6f5ce2fe08a58eb573472ea53792f007d6444a57
Alice Bonafé <alicecbonafe@gmail.com>
2026-08-18 05:45:25 -0300

    Datorum API exposure

--- src/datorum/__init__.py
+++ src/datorum/__init__.py
@@ -26,16 +26,133 @@ from .context.settings import (
     DocumentReference,
     DocumentContext,
 )
+from .context.exceptions import (
+    DocumentTypeError,
+    DocumentModelError,
+    DocumentHandlerError,
+    DocumentReferenceError,
+    DocumentReadingError,
+    DocumentWritingError,
+)
+from .context.commons.chat import (
+    ImageUrl,
+    TextPart,
+    ImagePart,
+    FunctionCall,
+    ToolFunction,
+    ToolCall,
+    SystemMessage,
+    UserMessage,
+    AssistantMessage,
+    ToolMessage,
+    FunctionMessage,
+    ChatMessage,
+    ChatHistory,
+)
+from .context.commons.markdown import (
+    MarkdownDocument,
+)
+from .binding.registry import (
+    register_resource_factory,
+    get_resource_factory,
+    resource,
+)
+from .binding.settings import (
+    ContextBindType,
+    ContextBind,
+    ResourceBind,
+)
+from .binding.binder import (
+    Binder,
+)
+from .binding.credentials import (
+    register_mapped_api_key_factory,
+)
+from .binding.exceptions import (
+    CredentialError,
+    KeyNotFoundError,
+    InvalidKeyNameError,
+    ResourceFactoryError,
+    BinderError,
+    ResourceBindingError,
+    ContextBindingError,
+)
+from .work.job import (
+    Broadcaster,
+    JobStatus,
+    Job,
+)
+from .work.worker import (
+    Worker,
+)
+from .work.exceptions import (
+    JobError,
+    JobStatusError,
+    WorkerError,
+    WorkerStartUpError,
+)
+from .tooling.registry import (
+    ToolBox,
+    FunctionDefinition,
+    ToolDefinition,
+    BaseToolBoxField,
+    ContextField,
+    ResourceField,
+    ToolBoxDefinition,
+    get_toolbox_definition,
+    tool,
+    toolbox,
+)
+from .tooling.settings import (
+    ToolBoxSetUp,
+    ToolKit,
+)
+from .tooling.worker import (
+    ToolWorker,
+)
+from .tooling.exceptions import (
+    ToolBoxRegistryError,
+    ToolWorkerError,
+)
+from .agency.settings import (
+    InferenceServiceProvider,
+    AgentRole,
+    AgencyKit,
+)
+from .agency.worker import (
+    AgentWorker,
+)
+from .agency.exceptions import (
+    AgentWorkerError,
+)
+from .plumbing.settings import (
+    BasePipelineStep,
+    HumanInteractionStep,
+    ToolStep,
+    AgentStep,
+    DecisionStep,
+    Pipeline,
+    PipeFlowState,
+    PipeFlow,
+    PlumbingKit,
+)
+from .plumbing.worker import (
+    PipelineWorker
+)
+from .plumbing.exceptions import (
+    PipelineWorkerError,
+)
 
 
 __all__ = [
+    # .core.settings
     "BaseDatorumSettings",
     "BaseDatorumPersistentSettings",
-
+    # .core.exceptions
     "DatorumBaseError",
     "SettingsError",
     "RegistryError",
-
+    # .context.registry
     "DocumentType",
     "DocumentModel",
     "DocumentHandler",
@@ -49,4 +166,102 @@ __all__ = [
     "doc_model",
     "serializer",
     "deserializer",
+    # .context.settings
+    "DocumentReference",
+    "DocumentContext",
+    # .context.exceptions
+    "DocumentTypeError",
+    "DocumentModelError",
+    "DocumentHandlerError",
+    "DocumentReferenceError",
+    "DocumentReadingError",
+    "DocumentWritingError",
+    # .context.commons.chat
+    "ImageUrl",
+    "TextPart",
+    "ImagePart",
+    "FunctionCall",
+    "ToolFunction",
+    "ToolCall",
+    "SystemMessage",
+    "UserMessage",
+    "AssistantMessage",
+    "ToolMessage",
+    "FunctionMessage",
+    "ChatMessage",
+    "ChatHistory",
+    # .context.commons.markdown
+    "MarkdownDocument",
+    # .binding.registry
+    "register_resource_factory",
+    "get_resource_factory",
+    "resource",
+    # .binding.settings
+    "ContextBindType",
+    "ContextBind",
+    "ResourceBind",
+    # .binding.binder
+    "Binder",
+    # .binding.credentials
+    "register_mapped_api_key_factory",
+    # .binding.exceptions
+    "CredentialError",
+    "KeyNotFoundError",
+    "InvalidKeyNameError",
+    "ResourceFactoryError",
+    "BinderError",
+    "ResourceBindingError",
+    "ContextBindingError",
+    # .work.job import (
+    "Broadcaster",
+    "JobStatus",
+    "Job",
+    # .work.worker import (
+    "Worker",
+    # .work.exceptions import (
+    "JobError",
+    "JobStatusError",
+    "WorkerError",
+    "WorkerStartUpError",
+    # .tooling.registry import (
+    "ToolBox",
+    "FunctionDefinition",
+    "ToolDefinition",
+    "BaseToolBoxField",
+    "ContextField",
+    "ResourceField",
+    "ToolBoxDefinition",
+    "get_toolbox_definition",
+    "tool",
+    "toolbox",
+    # .tooling.settings import (
+    "ToolBoxSetUp",
+    "ToolKit",
+    # .tooling.worker import (
+    "ToolWorker",
+    # .tooling.exceptions import (
+    "ToolBoxRegistryError",
+    "ToolWorkerError",
+    # .agency.settings import (
+    "InferenceServiceProvider",
+    "AgentRole",
+    "AgencyKit",
+    # .agency.worker import (
+    "AgentWorker",
+    # .agency.exceptions import (
+    "AgentWorkerError",
+    # .plumbing.settings import (
+    "BasePipelineStep",
+    "HumanInteractionStep",
+    "ToolStep",
+    "AgentStep",
+    "DecisionStep",
+    "Pipeline",
+    "PipeFlowState",
+    "PipeFlow",
+    "PlumbingKit",
+    # .plumbing.worker import (
+    "PipelineWorker",
+    # .plumbing.exceptions import (
+    "PipelineWorkerError",
 ]
